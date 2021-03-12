b9b97de9d5265094b9fd759164290b1f266650fc
i_kovshar <ikovshar91@gmail.com>
2021-03-12 16:18:05 +1200

    GachiProject

--- src/main/java/com/borcuhabobra/weatherbot/Weather.java
+++ src/main/java/com/borcuhabobra/weatherbot/Weather.java
@@ -1,16 +1,9 @@
 package com.borcuhabobra.weatherbot;
-
-import netscape.javascript.JSObject;
-import org.glassfish.jersey.server.Uri;
-import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
 public class Weather {
 
@@ -27,12 +20,6 @@ public class Weather {
 
         connection.setRequestMethod("GET");
         connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-
-        int responseCode = connection.getResponseCode();
-
-        if (responseCode == 404) {
-            throw new IllegalArgumentException();
-        }
 
         BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
